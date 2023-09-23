6522891f42b5581ebf395a49dcab51afb0cd767b
Alex <alekseichuk.a@gmail.com>
2023-09-23 23:13:34 +0200

    prettier config updated

--- src/app/article/components/article/article.component.ts
+++ src/app/article/components/article/article.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'md-article',
-  templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+    selector: 'md-article',
+    templateUrl: './article.component.html',
+    styleUrls: ['./article.component.css'],
 })
-export class ArticleComponent {
-
-}
+export class ArticleComponent {}
